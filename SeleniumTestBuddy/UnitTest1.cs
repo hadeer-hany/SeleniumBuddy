a6721b2440e70d98844d0187b73ade21a37e20be
hadeer-hany <hadeer.hany@moneyfellows.com>
2021-06-16 12:28:55 +0200

    chrome test

--- SeleniumTestBuddy/UnitTest1.cs
+++ SeleniumTestBuddy/UnitTest1.cs
@@ -18,9 +18,9 @@ namespace SeleniumTestBuddy
         [SetUp]
         public void Setup()
         {
-           // ChromeOptions chromeOptions = new ChromeOptions();
-            //chromeOptions.BinaryLocation = Path.GetFullPath(@"chromedriver_win32");
-            //driver = new ChromeDriver(chromeOptions);
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.BinaryLocation = Path.GetFullPath(@"chromedriver_win32");
+            driver = new ChromeDriver(chromeOptions);
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://stackoverflow.com/");
 
